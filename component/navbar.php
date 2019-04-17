2c12be9dbdaaf30f2bef8baf7b8fbd87f30e7196
nmtoan251998@gmail.com <nmtoan251998@gmail.com>
2019-04-17 20:32:37 +0700

    feature: user's components

---
+++ component/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-light bg-light navbar-expand-sm">
+    <a href="#" class="navbar-brand mr-5">
+        <img 
+            src="/public/img/" 
+            alt="logo" 
+            width="30" 
+            height="30"
+            class="rounded-circle">
+    </a>
+    <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/page/manageBlog.php">Manage blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/page/newBlog.php">New blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/page/contributor.php">Contributors</a>
+            </li>
+        </ul>
+    </div>
+</nav>
