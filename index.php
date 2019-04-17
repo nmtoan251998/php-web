5e6cfcffc902c47c163d97bbf078b3054b0e922d
nmtoan251998@gmail.com <nmtoan251998@gmail.com>
2019-04-17 20:33:53 +0700

    user's index page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include "config.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap 4.0.0 -->
+    <link rel="stylesheet" type="text/css" href="/public/css/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/bootstrap-fix.css">
+
+    <title>Welcome to Noooat blogspot</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <?php include "component/navbar.php"?>
+    </div>
+   
+    <script src="/public/js/jquery-3.4.0.min.js"></script>
+    <script src="/public/js/popper.min.js"></script>
+    <script src="/public/js/bootstrap4/bootstrap.min.js"></script>
+    <script src="/public/js/navbar.js"></script>
+</body>
+</html>
