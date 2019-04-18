dc0c2afb5c55ec34ac8ef50c012aa681de11fae9
nmtoan251998@gmail.com <nmtoan251998@gmail.com>
2019-04-18 23:36:29 +0700

    feature: delete existed blog

---
+++ controller/admin/manageBlog.delete.php
@@ -0,0 +1,28 @@
+<?php require "../../config/db.config.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../public/css/bootstrap4/bootstrap.min.css">
+</head>
+<body>
+    <?php
+        $query = $_SERVER["QUERY_STRING"];
+        $id = (int)substr($query, -1); 
+
+        $conn = getDb();
+        $sql = "DELETE FROM post WHERE blog_id=$id";
+
+        if(mysqli_query($conn, $sql)){
+            ?>
+            <script>
+                alert("Deleted!");
+                window.location.href="/view/admin/page/manageBlog.php"
+            </script>
+        <?php 
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+        }
+    ?>
+</body>
+</html>
