b78c6ebb354f5aefef8ca80ea2dff32996a8b788
nmtoan251998@gmail.com <nmtoan251998@gmail.com>
2019-04-18 23:20:06 +0700

    feature: insert new blog

---
+++ controller/admin/newBlog.insert.php
@@ -0,0 +1,38 @@
+<?php require "../../config/db.config.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../public/css/bootstrap4/bootstrap.min.css">
+</head>
+<body>
+    <?php
+        if(isset($_POST['submit'])) { 
+            // retrieve the form data by using the element's name attributes value as key 
+            $blog_id = $_POST['blog_id']; 
+            $title = $_POST['title']; 
+            $description = $_POST['description'];    
+            $author = $_POST['author']; 
+            $post_date = $_POST['post_date']; 
+
+            $conn = getDb();
+            $sql = "INSERT INTO post VALUES($blog_id, '$title', '$description', '$author', '$post_date')";
+            
+            if(mysqli_query($conn, $sql)){
+                ?>
+                <script>
+                    setTimeout(() => {
+                        window.location.href="/view/admin/index.php"
+                    }, 3000);
+                </script>
+            <?php 
+            } else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+            }
+            
+        } else {
+            echo "Fail";
+        }
+    ?>
+</body>
+</html>
